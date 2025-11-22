1dd0c37a43f428b91cc0b9569ef01b06656b3fab
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-22 22:47:16 +0800

    FInished API Endpoint for login.

--- Backend/Controller/HomePageController.cs
+++ Backend/Controller/HomePageController.cs
@@ -1,11 +1,35 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using Backend.DTOs.Auth;
+using Backend.Services.Authentication;
+using Backend.DTOs;
 namespace Backend.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
     public class HomePageController : ControllerBase
     {
+        public async Task<IActionResult> login(LoginDTO user, [FromServices] AuthServices service)
+        {
+            try
+            {
+                var result = await service.CheckUserService(user);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new APIResponseDTO<object>()
+                {
+                    success = false,
+                    StatusCode = 500,
+                    message = "Internal Error",
+                    Errors = new List<string>()
+                    {
+                        e.Message
+                    }
+                });
+            }
+            
+        }
     }
 }
