d81551d3acb63e1154e557ea4077c70b732187ea
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-23 21:30:50 +0800

    created method for add user

--- Backend/Services/ManageUserService.cs
+++ Backend/Services/ManageUserService.cs
@@ -1,13 +1,57 @@
 using Backend.DTOs;
 using Backend.DTOs.ManageUserDTO;
+using Backend.Repository;
 
 namespace Backend.Services;
 
 public class ManageUserService
 {
-    private readonly 
+    private readonly ManageUserRepository repository;
+
+    public ManageUserService(ManageUserRepository _repository)
+    {
+        repository = _repository;
+    }
+
     public async Task<APIResponseDTO<string>> AddUserService(AddUserDTO user)
     {
-        
+        if (user.password != user.confirmPassword)
+        {
+            return new APIResponseDTO<string>()
+            {
+                success = false,
+                StatusCode = 404,
+                message = "Retry Password",
+                Errors = new List<string>()
+                {
+                    "Password mismatched."
+                }
+            };
+        }
+
+        try
+        {
+            var result = await repository.AddUserRepository(user);
+            return new APIResponseDTO<string>()
+            {
+                success = true,
+                StatusCode = 200,
+                message = "User successfully created."
+            };
+
+        }
+        catch (Exception e)
+        {
+            return new APIResponseDTO<string>()
+            {
+                success = false,
+                StatusCode = 500,
+                message = "Error creating user.",
+                Errors = new List<string>()
+                {
+                    e.Message
+                }
+            };
+        }
     }
 }
