b76a1620ae3f58b633a9a5c790ec0a7ccf98cfc6
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-23 12:35:11 +0800

    api controller for user management

---
+++ Backend/Controller/manage-userController.cs
@@ -0,0 +1,16 @@
+using Backend.DTOs.ManageUserDTO;
+using Microsoft.AspNetCore.Mvc;
+using Backend.DTOs;
+using Backend.Services;
+namespace Backend.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class manage_userController : ControllerBase
+    {
+        public async Task<APIResponseDTO<SuccessResponseDTO>> add(AddUserDTO user, [FromServices] ManageUserService service)
+        {
+            var result = await service.AddUserService();
+        }
+    }
+}
