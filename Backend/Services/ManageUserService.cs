103bad1e694e4d3b986af3937b28c66864ec15bc
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-23 12:35:25 +0800

    user management services

---
+++ Backend/Services/ManageUserService.cs
@@ -0,0 +1,13 @@
+using Backend.DTOs;
+using Backend.DTOs.ManageUserDTO;
+
+namespace Backend.Services;
+
+public class ManageUserService
+{
+    private readonly 
+    public async Task<APIResponseDTO<string>> AddUserService(AddUserDTO user)
+    {
+        
+    }
+}
