98d26aad1e6018822384a58d19324e5651f204b8
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-23 21:30:26 +0800

    Add confirmPassword property

--- Backend/DTOs/ManageUserDTO/AddUserDTO.cs
+++ Backend/DTOs/ManageUserDTO/AddUserDTO.cs
@@ -5,6 +5,7 @@ public class AddUserDTO
     public string username { get; set; }
     public string email { get; set; }
     public string password { get; set; }
+    public string confirmPassword { get; set; }
     public string firstName { get; set; }
     public string middleName { get; set; }
     public string surName { get; set; }
