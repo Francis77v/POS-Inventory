bf6f92e4a56ba920de8d93bcec306ee17aa28dcf
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-24 20:39:51 +0800

    Applied thin controller in auth controller

--- Backend/Controller/HomePageController.cs
+++ Backend/Controller/HomePageController.cs
@@ -10,27 +10,11 @@ namespace Backend.Controller
     public class HomePageController : ControllerBase
     {
         [HttpPost("login")]
-        public async Task<IActionResult> login(LoginDTO user, [FromServices] AuthServices service)
+        public async Task<IActionResult> Login(LoginDTO user, [FromServices] AuthServices service)
         {
-            try
-            {
-                var result = await service.CheckUserService(user);
-                return Ok(result);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, new APIResponseDTO<object>()
-                {
-                    success = false,
-                    StatusCode = 500,
-                    message = "Internal Error",
-                    Errors = new List<string>()
-                    {
-                        e.Message
-                    }
-                });
-            }
-            
+            var result = await service.CheckUserService(user);
+            return StatusCode(result.StatusCode, result);
         }
+
     }
 }
