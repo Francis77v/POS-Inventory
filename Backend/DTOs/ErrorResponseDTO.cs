3e2840057f3723ed3fb30ff374be480bc746693e
Francis77v <floresfrancisjoseph28@gmail.com>
2025-11-25 15:32:44 +0800

    Add properties on error dto

--- Backend/DTOs/ErrorResponseDTO.cs
+++ Backend/DTOs/ErrorResponseDTO.cs
@@ -1,6 +1,7 @@
 namespace Backend.DTOs;
 
-public class ErrorResponseDTO
+public class ErrorResponseDTO<T>
 {
-    
+    public T? Data { get; set; }
+    public List<string>? Errors { get; set; }
 }
