4bd2430f66fa3b3ff4e14d01e49d723b616c4ace
Francis77v <floresfrancisjoseph28@gmail.com>
2025-11-25 15:33:47 +0800

    Add context field and constructor. Add method for checking if product exts and add product method.

--- Backend/Repository/InventoryRepository.cs
+++ Backend/Repository/InventoryRepository.cs
@@ -1,6 +1,28 @@
-﻿namespace Backend.Repository;
+﻿using Backend.Context;
+using Backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repository;
 
 public class InventoryRepository
 {
-    
+    private readonly MyDbContext _context;
+
+    public InventoryRepository(MyDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> CheckProductIfExist(string product)
+    {
+        var result = await _context.Product.AnyAsync(p => p.productName == product);
+        if (result) return true;
+        return false;
+    }
+
+    public async Task AddProductRepository(Products product)
+    {
+        await _context.Product.AddAsync(product);
+        await _context.SaveChangesAsync();
+    }
 }
