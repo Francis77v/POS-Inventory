0a58b08687ebde4f9e7f2117d24ccf01cfdbc831
Francis77v <floresfrancisjoseph28@gmail.com>
2025-11-26 10:54:39 +0800

    Create a repository crud for category model

---
+++ Backend/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Backend.Context;
+using Backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repository;
+
+public class CategoryRepository
+{
+    private readonly MyDbContext _context;
+
+    public CategoryRepository(MyDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<string> FetchCategory(int id)
+    {
+        var result = await _context.category
+            .Where(c => c.Id == id)
+            .Select(c => c.categoryName)
+            .FirstOrDefaultAsync();
+
+        return result;
+    }
+}
