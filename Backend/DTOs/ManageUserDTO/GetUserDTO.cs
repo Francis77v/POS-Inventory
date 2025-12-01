03199c9e3a721a114a5a1bfd67161ec471989dd6
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-01 21:49:51 +0800

    create get user dto for fetchi user

---
+++ Backend/DTOs/ManageUserDTO/GetUserDTO.cs
@@ -0,0 +1,12 @@
+namespace Backend.DTOs.ManageUserDTO;
+
+public class GetUserDTO
+{
+    public string Id { get; set; }
+    public string firstName { get; set; }
+    public string? middleName { get; set; }
+    public string surName { get; set; }
+    public string username { get; set; }
+    public string password { get; set; }
+    // public string role { get; set; }
+}
