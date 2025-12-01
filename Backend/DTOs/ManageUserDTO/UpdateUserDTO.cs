0b5e3a385cd26e287e2b45f861cfa47a16730c21
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-01 22:58:21 +0800

    Create DTO for update user functionality

---
+++ Backend/DTOs/ManageUserDTO/UpdateUserDTO.cs
@@ -0,0 +1,9 @@
+namespace Backend.DTOs.ManageUserDTO;
+
+public class UpdateUserDTO
+{
+    public string? firstName { get; set; }
+    public string? middleName { get; set; }
+    public string? surName { get; set; }
+    public string? userName { get; set; }
+}
