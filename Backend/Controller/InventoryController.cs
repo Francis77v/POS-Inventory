85872a47dacfe39575b9f40b665e613e6b35fd51
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-11 21:55:25 +0800

    add allow anonymous for testing

--- Backend/Controller/InventoryController.cs
+++ Backend/Controller/InventoryController.cs
@@ -1,5 +1,6 @@
 using Backend.DTOs.InventoryDTO;
 using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controller
@@ -34,7 +35,8 @@ namespace Backend.Controller
             var result = await _services.DeleteProductService(SKU);
             return StatusCode(result.StatusCode, result);
         }
-
+        
+        [AllowAnonymous]
         [HttpGet("display")]
         public async Task<IActionResult> FetchAllProduct()
         {
