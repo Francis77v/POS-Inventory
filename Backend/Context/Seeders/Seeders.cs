7c38bb7f944c1462382fb9ee01ef7436fd433441
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-12 17:14:24 +0800

    Add new seed data

--- Backend/Context/Seeders/Seeders.cs
+++ Backend/Context/Seeders/Seeders.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Backend.Model;
 namespace Backend.Context.Seeders;
 
 public static class Seeders
 {
-    public static void RoleSeeder(this ModelBuilder modelBuilder)
+    public static void DataSeeder(this ModelBuilder modelBuilder)
     {
+        var hasher = new PasswordHasher<Users>();
         modelBuilder.Entity<IdentityRole>().HasData(
             new IdentityRole
             {
@@ -20,6 +22,30 @@ public static class Seeders
                 Name = "Cashier",
                 NormalizedName = "CASHIER",
                 ConcurrencyStamp = Guid.NewGuid().ToString()
+            }
+        );
+
+        var user = new Users
+        {
+            Id = Guid.NewGuid().ToString(),
+            FirstName = "Darvin",
+            MiddleName = "Oma",
+            SurName = "Palma",
+            Email = "darvin@email.com",
+            NormalizedEmail = "DARVIN@EMAIL.COM",
+            UserName = "darvin123",
+            NormalizedUserName = "DARVIN123"
+        };
+
+        user.PasswordHash = hasher.HashPassword(user, "Darvin123!");
+
+        modelBuilder.Entity<Users>().HasData(user);
+
+        modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+            new IdentityUserRole<string>
+            {
+                RoleId = "1",
+                UserId = user.Id
             }
         );
     }   
